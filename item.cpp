99f39a19fab5c6fc566e2b248142e9e9e04fd05f
Emil Segerbäck <emilsegerback@gmail.com>
2019-01-26 10:20:39 +0100

    Add randomization of spawned item positions

--- item.cpp
+++ item.cpp
@@ -1,12 +1,23 @@
+#include <math.h>
 #include "item.hpp"
+
+const int SPAWN_RADIUS = 150;
 
 void spawn_item(std::vector<Item*>& items) {
     if (items.size() < MAX_ITEMS) {
         Item* item = new Item;
 
+        sf::Vector2f center(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+        float random_angle = float(rand() % 180) / M_PI;
+        int random_radius = rand() % SPAWN_RADIUS;
+        sf::Vector2f offset(
+            random_radius * cos(random_angle),
+            random_radius * sin(random_angle)
+        );
+
         sf::CircleShape shape(10, 3);
         shape.setOrigin(10, 10);
-        shape.setPosition(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+        shape.setPosition(center + offset);
         shape.setFillColor(sf::Color(200, 255, 255));
 
         item->shape = shape;
